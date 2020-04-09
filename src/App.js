daa6e0f31e721f8fa74a3135d827103b2986bc04
Thomas Culdaut <thomas@wildcodeschool.fr>
2020-04-09 11:35:16 +0200

    instructions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ParticipantList from "./ParticipantList";
 
+// Instructions pour 16H30
+
+// Fork le repo
+// clone le repo
+// cd repo
+// npm install
+
+// Afficher la liste ds participants avec leur token
+// En utilisant l'export de participants.js
+// modifier ParticipantList
+// dans ParticipantList il va falloir faire un map
+// un map sur quoi ?
+// un map qui renvoi quoi ?
+
 // Quel(s) composant(s) ai je besoin de créer ?
 // Composant que je veux afficher est une liste de participants
 // Participants
